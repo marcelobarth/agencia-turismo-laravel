8aafcb7c52d4afe5f9ea14dbd162a095820c37d4
marcelobarth <marcelinhobarth@hotmail.com>
2020-03-03 15:34:41 -0300

    Recuperar Senha

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('panel', 'Panel\PanelController@index');
 Route::get('promocoes', 'Site\SiteController@promotions')->name('promotions');
 
 Route::get('/', 'Site\SiteController@index');
+
+Auth::routes();
