0fb393d9564545cdd20b0ef945e4d813ead23053
marcelobarth <marcelinhobarth@hotmail.com>
2020-03-17 13:22:19 -0300

    Aula 6

--- app/Http/Controllers/Panel/BrandController.php
+++ app/Http/Controllers/Panel/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Panel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
+use App\Http\Requests\BrandStoreUpdateFormRequest;
 
 class BrandController extends Controller
 {
@@ -47,7 +48,7 @@ class BrandController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BrandStoreUpdateFormRequest $request)
     {
         $dataForm = $request->all();
 
