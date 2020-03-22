d24dae801990ab58ba1e81a61b637d835a624bb5
marcelobarth <marcelinhobarth@hotmail.com>
2020-03-22 11:04:23 -0300

    Aula 3.9

--- resources/views/panel/brands/create-edit.blade.php
+++ resources/views/panel/brands/create-edit.blade.php
@@ -24,21 +24,22 @@
     </div>
     @endif
 
-    @if(isset($brand))
-    <form class="form form-search form-ds" action="{{route('brands.update', $brand->id)}}" method="post">
-        {!! method_field('PUT') !!}
-        @else
-        <form class="form form-search form-ds" action="{{route('brands.store')}}" method="POST">
-            @endif
-            {!! csrf_field() !!}
-            <div class="form-group">
-                <input type="text" value="{{old('name')}}" name="name" placeholder="Nome:" class="form-control">
-            </div>
+    @if( isset($brand) )
+    <!--<form class="form form-search form-ds" action="{{route('brands.update', $brand->id)}}" method="POST">-->
+    {!! Form::model($brand, ['route' => ['brands.update', $brand->id], 'class' => 'form form-search form-ds', 'method' => 'PUT']) !!}
+    @else
+    <!--<form class="form form-search form-ds" action="{{route('brands.store')}}" method="POST">-->
+    {!! Form::open(['route' => 'brands.store', 'class' => 'form form-search form-ds']) !!}
+    @endif
+    <div class="form-group">
+        <!-- <input type="text" value="{{old('name')}}" name="name" placeholder="Nome:" class="form-control"> -->
+        {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nome']) !!}
+    </div>
 
-            <div class="form-group">
-                <button class="btn btn-search">Enviar</button>
-            </div>
-        </form>
+    <div class="form-group">
+        <button class="btn btn-search">Enviar</button>
+    </div>
+    {!! Form::close() !!}
 
 </div>
 <!--Content Dinâmico-->
